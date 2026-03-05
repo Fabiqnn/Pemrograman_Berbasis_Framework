e4aea400ca919754c790731ece387625acf3761a
Fabiqnn <arema20056@gmail.com>
2026-03-05 14:10:26 +0700

    Praktikum 3 - Jobsheet 7

--- src/pages/views/produk/index.tsx
+++ src/pages/views/produk/index.tsx
@@ -1,18 +1,53 @@
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
-export default function TampilanProduk(){
-    const {push} = useRouter();
-    useEffect(() => {
-        const loginStatus = localStorage.getItem("isLogin")
-        if(!loginStatus) {
-            push("/auth/login")
-        }
-    },[]);
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    size: string;
+    category: string;
+}
 
-    return(
-        <>
-            <div>Produk User Page</div>
-        </>
+export default function TampilanProduk() {
+    // const { push } = useRouter();
+    // useEffect(() => {
+    //     const loginStatus = localStorage.getItem("isLogin")
+    //     if (!loginStatus) {
+    //         push("/auth/login")
+    //     }
+    // }, []);
+    const [products, setProducts] = useState<ProductType[]>([]);
+
+    const fetchProduct = async () => {
+        try {
+            const response = await fetch("/api/produk");
+            const responseData = await response.json();
+            setProducts(responseData.data)
+        } catch (e) {
+            console.error("Error fetching produk: ", e)
+        }
+    }
+
+    useEffect(() => {
+        fetchProduct();
+    }, []);
+
+    return (
+        <div>
+            <h1 className="text-xl font-bold">Daftar Produk</h1><br />
+
+            <button onClick={fetchProduct} className="bg-blue-500 text-white px-4 py-2 rounded">Refresh Data</button>
+
+            {products.map((products:ProductType) => (
+                <div key={products.id}>
+                    <br />
+                    <h2 className="text-lg font-bold">{products.name}</h2>
+                    <p>Harga: {products.price}</p>
+                    <p>Ukuran: {products.size}</p>
+                    <p>Kategori: {products.category}</p>
+                </div>
+            ))}
+        </div>
     )
 }
