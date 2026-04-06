a388f396e8f417ac88ffcf86714c6af27b5e4ec9
Fabiqnn <arema20056@gmail.com>
2026-04-07 03:56:46 +0700

    Praktikum 5 - Jobsheet 13

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,8 +1,12 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import TampilanProduk from "@/views/produk";
 import useSWR from "swr";
 import fetcher from "@/utils/swr/fetcher";
 
 export default function halamanProduk() {
+    const {push} = useRouter();
+    const [products, setProducts] = useState([]);
 
     const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
