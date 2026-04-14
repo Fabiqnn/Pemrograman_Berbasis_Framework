97442b1bb4e7055067debe7289020aebe557b659
Fabiqnn <arema20056@gmail.com>
2026-04-14 19:07:26 +0700

    Praktikum 6 - Jobsheet 15

--- src/utils/db/servicefirebase.ts
+++ src/utils/db/servicefirebase.ts
@@ -1,9 +1,20 @@
-import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore"
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    doc,
+    Firestore,
+    getDoc,
+    query,
+    addDoc,
+    where
+} from "firebase/firestore"
 import app from "./firebase"
+import bcrypt from "bcrypt"
 
 const db = getFirestore(app)
 
-export async function retrieveProducts(collectionName:string) {
+export async function retrieveProducts(collectionName: string) {
     const snapshot = await getDocs(collection(db, collectionName));
     const data = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -16,4 +27,48 @@ export async function retrieveDataByID(collectionName: string, id: string) {
     const snapshot = await getDoc(doc(db, collectionName, id));
     const data = snapshot.data();
     return data;
+}
+
+export async function signUp(
+    userData: {
+        email: string;
+        fullname: string;
+        password: string;
+        role?: string;
+    },
+    callback: Function,
+) {
+    const q = query(
+        collection(db, "users"),
+        where("email", "==", userData.email)
+    );
+    const querySnapshot = await getDocs(q);
+    const data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+
+    if (data.length === 0) {
+        userData.password = await bcrypt.hash(userData.password, 10);
+        userData.role = "user";
+        await addDoc(collection(db, "users"), userData)
+            .then(() => {
+                callback({
+                    status: "success",
+                    message: "User registered successfully"
+                });
+            })
+            .catch((e) => {
+                callback({
+                    status: "error",
+                    message: e.message
+                });
+            })
+    } else {
+        callback({
+            status: "Error",
+            message: "User already exist"
+        });
+
+    }
 }
--- src/views/auth/register/index.tsx
+++ src/views/auth/register/index.tsx
@@ -1,26 +1,87 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "./register.module.scss"
 
 export default function TampilanRegister() {
+    const [isLoading, setIsLoading] = useState(false);
+    const {push} = useRouter();
+    const [error, setError] = useState({email:"", password:""});
+    const [serverErr, setServerErr] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setIsLoading(true);
+        setError({email:"", password:""});
+        const form = event.currentTarget;
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get("email") as string;
+        const fullname = formData.get("fullname") as string;
+        const password = formData.get("password") as string;
+        if (!email) {
+            setIsLoading(false);
+            setError({
+                email:"Field email tidak boleh kosong",
+                password:""
+            });
+            return;
+        } 
+        
+        if (password.length < 6) {
+            setIsLoading(false);
+            setError({
+                email:"",
+                password:"Password tidak boleh kurang dari 6 karakter!"
+            });
+            return;
+        }
+        const response = await fetch("/api/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email, fullname, password})
+        });
+        if (response.status === 200) {
+            form.reset();
+            setIsLoading(false);
+            push("/auth/login");
+        } else {
+            setIsLoading(false);
+            setServerErr(
+                response.status === 400 ? "Email already exists" : "An error occured",
+            );
+        }
+    };
+    
     return(
         <div className={styles.register}>
-            <h1>Halaman Register</h1>
-            <form action="" className={styles.form_regis}>
-                <div className={styles.input}>
-                    <label htmlFor="nama">Nama Lengkap</label>
-                    <input type="text" id="nama"/>
-                </div>
-                <div className={styles.input}>
-                    <label htmlFor="noInd">No Induk</label>
-                    <input type="text" id="noInd"/>
-                </div>
-                <div className={styles.input}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password"/>
-                </div>
-                <button type="submit">Daftar</button>
-            </form>
-            <Link href={"/auth/login"}>Ke Halaman Login</Link>
+            {serverErr && <p className={styles.register__error}>{serverErr}</p>}
+            <h1 className={styles.register__title}>Halaman Register</h1>
+            <div className={styles.register__form}>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="email" className={styles.register__form__item__label}>Email</label>
+                        <input type="email" id="email" name="email" placeholder="Email" className={styles.register__form__item__input}/>
+                        {error.email && <p className={styles.register__error}>{error.email}</p>}
+                    </div>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="fullname" className={styles.register__form__item__label}>Fullname</label>
+                        <input type="text" id="fullname" name="fullname" placeholder="Fullname" className={styles.register__form__item__input} required />
+                    </div>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="password" className={styles.register__form__item__label}>Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password" className={styles.register__form__item__input} required />
+                        {error.password && <p className={styles.register__error}>{error.password}</p>}
+                    </div>
+                    <button type="submit" className={styles.register__form__item__button} disabled={isLoading}>
+                        {isLoading ? "Loading..." : "Register"}
+                    </button>
+                </form>
+                <br />
+                {serverErr && <p className={styles.register__form__item__text}>{serverErr}</p>}
+                <p className={styles.register__form__item__text}>Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link></p>
+            </div>
         </div>
     )
 }
