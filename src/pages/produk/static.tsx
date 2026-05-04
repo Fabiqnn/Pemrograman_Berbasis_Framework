4f9b1fd408d52004b53bff3b48ddd56e9c394dde
Fabiqnn <arema20056@gmail.com>
2026-05-05 01:41:28 +0700

    Uncomment code in static page for product display and retrieval

--- src/pages/produk/static.tsx
+++ src/pages/produk/static.tsx
@@ -1,24 +1,24 @@
-// import TampilanProduk from "@/views/produk";
-// import { ProductType } from "@/types/Product.type";
-// import { retrieveProducts } from "@/utils/db/servicefirebase";
+import TampilanProduk from "@/views/produk";
+import { ProductType } from "@/types/Product.type";
+import { retrieveProducts } from "@/utils/db/servicefirebase";
 
-// export default function halamanProdukStatic(props:{products:ProductType[]}) {
-//     const {products} = props;
-//     return (
-//         <div>
-//             <h1>Halaman Produk Static</h1>
-//             <TampilanProduk products={products}/>
-//         </div>
-//     )
-// }
+export default function halamanProdukStatic(props:{products:ProductType[]}) {
+    const {products} = props;
+    return (
+        <div>
+            <h1>Halaman Produk Static</h1>
+            <TampilanProduk products={products}/>
+        </div>
+    )
+}
 
-// export async function getStaticProps() {
-//     const products = await retrieveProducts("products") as ProductType[];
+export async function getStaticProps() {
+    const products = await retrieveProducts("products") as ProductType[];
 
-//     return {
-//         props: {
-//             products,
-//         },
-//         revalidate: 10,
-//     }
-// }
+    return {
+        props: {
+            products,
+        },
+        revalidate: 10,
+    }
+}
